ec2acb774590740bf4446ebfb5ab3cc473e3ccb9
nnguyen <55342025+nhi-nguyen-csis@users.noreply.github.com>
2022-10-15 16:16:54 -0700

    Correct Question 11, 12, 13

--- SQL Exercise/The Join Operation.sql
+++ SQL Exercise/The Join Operation.sql
@@ -78,7 +78,7 @@ SELECT matchid, mdate, COUNT(*) FROM
 SELECT matchid, mdate, COUNT(*) FROM
 	game JOIN goal ON (id = matchid)
 	WHERE teamid = 'GER'
-	GROUP BY matchid
+	GROUP BY matchid, mdate
 
 //13. 
 //List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
@@ -88,13 +88,13 @@ SELECT matchid, mdate, COUNT(*) FROM
 //10 June 2012	IRL	1	CRO	3
 //...
 //Notice in the query given every goal is listed. If it was a team1 goal then a 1 appears in score1, otherwise there is a 0. You could SUM this column to get a count of the goals scored by team1. Sort your result by mdate, matchid, team1 and team2.
-SELECT mdate, 
-	   team1, 
-	   SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1, 
-	   team2, 
-	   SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2 FROM
-	game LEFT JOIN goal ON (id = matchid)
-	GROUP BY id
-	ORDER BY mdate, matchid, team1, team2
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+  team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+  FROM game LEFT JOIN goal ON (id = matchid)
+GROUP BY mdate, matchid, team1, team2
+ORDER BY mdate, matchid, team1, team2
 
 	
